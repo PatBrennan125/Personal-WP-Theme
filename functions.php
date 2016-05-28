916e591ee37d8ea59a22d63fe9a116ca0e83077c
Patrick Brennan <patbrennan125@gmail.com>
2016-05-28 21:15:53 +0100

    Setting up theme files

---
+++ functions.php
@@ -0,0 +1,10 @@
+/*
+Theme Name: Personal Site
+Author: Patrick Brennan
+Author URI: http://irishdealsdaily.com
+Description: Responsive theme for inderviduals looking to showcase their work or business online.
+Version: 1.0
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags: Responsive
+*/
