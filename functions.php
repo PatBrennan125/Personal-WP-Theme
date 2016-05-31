37734dcec78a50f3a7b0313f09929970a64d63c3
Patrick Brennan <patbrennan125@gmail.com>
2016-05-31 14:35:07 +0100

    Added new files and updated functions

--- functions.php
+++ functions.php
@@ -1,10 +1,35 @@
-/*
-Theme Name: Personal Site
-Author: Patrick Brennan
-Author URI: http://irishdealsdaily.com
-Description: Responsive theme for inderviduals looking to showcase their work or business online.
-Version: 1.0
-License: GNU General Public License v2 or later
-License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Tags: Responsive
-*/
+<?php
+
+add_theme_support('menus');
+add_theme_support('post-thumbnails');
+
+function register_theme_menus() {
+	register_nav_menus(
+		array(
+			'primary-menu' => __('Primary Menu')
+		)
+	);
+}
+
+add_action('init', 'register_theme_menus');
+
+function personal_theme_styles() {
+
+	wp_enqueue_style('foundation_css', get_template_directory_uri() . '/css/foundation.css');
+	//wp_enqueue_style('normalize_css', get_template_directory_uri() . '/css/normalize.css');
+	wp_enqueue_style('font_oswald', 'https://fonts.googleapis.com/css?family=Oswald:400,700');
+	wp_enqueue_style('main_css', get_template_directory_uri() . '/style.css');
+}
+
+add_action('wp_enqueue_scripts', 'personal_theme_styles');
+
+function personal_theme_js() {
+
+	wp_enqueue_script('modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '','', false);
+	wp_enqueue_script('foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'),'', true);
+	wp_enqueue_script('app_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'),'', true);
+}
+
+add_action('wp_enqueue_scripts', 'personal_theme_js');
+
+?>
---
+++ js/app.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+
+	$( ".nav-toggle" ).click(function() {
+  $(this).toggleClass("open");
+  $("nav").fadeToggle(100);
+
+  return false;
+});
+
+});
